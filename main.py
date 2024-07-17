26db2bd11340b5e14fe3c08b678cc2a9d111da40
PushkargithubCSE <155304664+PushkargithubCSE@users.noreply.github.com>
2024-07-17 17:03:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import streamlit as st
 from datetime import date
 import yfinance as yf
-from prophet import Prophet
-from prophet.plot import plot_plotly
+from neuralprophet import NeuralProphet
 from plotly import graph_objs as go
 
 START = "2015-01-01"
@@ -10,56 +9,39 @@ TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title("Stock Prediction App")
 
-stocks = ("AAPL", "GOOG", "MSFT", "TSLA","RVNL.NS")
+stocks = ("AAPL", "GOOG", "MSFT", "TSLA")
 selected_stock = st.selectbox("Select dataset for prediction", stocks)
 
 n_years = st.slider("Years of prediction:", 1, 5)
 periods = n_years * 365
 
-@st.cache_data
+@st.cache
 def load_data(ticker):
     data = yf.download(ticker, START, TODAY)
     data.reset_index(inplace=True)
     return data
 
-data_load_state = st.text("Loading data...")
+data_load_state = st.text("Load data ...")
 data = load_data(selected_stock)
-data_load_state.text("Loading data...done!")
+data_load_state.text("Loading Data successfully done")
 
-st.subheader('Raw Data')
+st.subheader('Raw data')
 st.write(data.tail())
 
-def plot_raw_data():
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data['Date'],y=data['Open'],name = 'stock_open'))
-    fig.add_trace(go.Scatter(x=data['Date'],y=data['Close'],name = 'stock_close'))
-    fig.layout.update(title_text="Time series data",xaxis_rangeslider_visible = True)
-    st.plotly_chart(fig)
+# Prepare data for NeuralProphet
+df_train = data[['Date', 'Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
-
-plot_raw_data()
-
-#forecasting
-
-df_train = data[['Date','Close']]
-df_train = df_train.rename(columns={"Date":"ds","Close":"y"})
-
-m=Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods=periods)
-forecast = m.predict(future)
+model = NeuralProphet()
+model.fit(df_train, freq='D')
+future = model.make_future_dataframe(df_train, periods=periods)
+forecast = model.predict(future)
 
 st.subheader('Forecast data')
 st.write(forecast.tail())
 
-
-st.write('forecast data')
-fig1 = plot_plotly(m, forecast)
+fig1 = model.plot(forecast)
 st.plotly_chart(fig1)
-
-st.write('forecast components')
-fig2 = m.plot_components(forecast)
-st.write(fig2)
 
 
 
